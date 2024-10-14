15b05498aef94aa90d57a14c4d7c8cebc8a32a19
Shreyas Shriyan <shreyshriyan@gmail.com>
2024-10-14 22:08:55 +0530

    tauri 2.0 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
+
+  const [items, setItems] = useState([""]);
+
   const [name, setName] = useState("");
 
   async function greet() {
@@ -11,7 +14,18 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
-  return <div className=""></div>;
+  return (
+    <div className="container">
+      <input onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setItems([...items, name])}>Add</button>
+      <div>
+        {items.map((item) => {
+          return <div style={{ padding: "10px" }}>{item}</div>;
+        })}
+      </div>
+      <p>{greetMsg}</p>
+    </div>
+  );
 }
 
 export default App;
